a0d3b87940694cf61b1eab3fb58ff88fc76c6996
danibuia <daniel.buia@smartboxdigital.com>
2023-12-14 14:57:43 +0200

    Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import "./App.css";
 import Views from "./views";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./utils/config/theme";
+import useThemeToggle from "./hooks/use-theme-toggle";
+import ThemeContext from "./utils/context/theme";
 
 const App: FC = () => {
-  return <Views />;
+  const [themeMode, toggleThemeMode] = useThemeToggle();
+  const themeContext = useMemo(
+    () => ({ themeMode, toggleThemeMode }),
+    [themeMode, toggleThemeMode]
+  );
+  return (
+    <ThemeProvider theme={theme[themeMode]}>
+      <ThemeContext.Provider value={themeContext}>
+        <CssBaseline />
+        <Views />
+      </ThemeContext.Provider>
+    </ThemeProvider>
+  );
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    Unfonts({
+      google: {
+        families: ["Montserrat"],
+      },
+    }),
+    react(),
+  ],
+});
